00b9f9999e387191402e2a454718b07fea21448d
Jet Xu <jet.xu@ccconceptcn.com>
2019-12-17 11:36:33 +0800

    仅用于学习的地址

--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -8,7 +8,9 @@ export const API_CONFIG = new InjectionToken('ApiConfigToken');
     
   ],
   providers: [
-    { provide: API_CONFIG, useValue: 'http://localhost:3000/' },
+    // { provide: API_CONFIG, useValue: 'http://localhost:3000/' },
+    { provide: API_CONFIG, useValue: 'http://49.232.96.54:7777/' }, // 可以用(仅用于学习)
+    // { provide: API_CONFIG, useValue: 'http://musicapi.leanapp.cn/' },
     // { provide: API_CONFIG, useValue: 'http://u-to-world.com:3000/' }
   ]
 })
