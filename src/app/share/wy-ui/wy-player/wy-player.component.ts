4f047949322ae734d8dd4ce407794c3837e19297
Jet Xu <jet.xu@ccconceptcn.com>
2019-12-17 10:20:38 +0800

    歌曲的播放，切换上下首

--- src/app/share/wy-ui/wy-player/wy-player.component.ts
+++ src/app/share/wy-ui/wy-player/wy-player.component.ts
@@ -4,6 +4,7 @@ import { AppStoreModule } from '../../../store/index';
 import { getSongList, getPlayList, getCurrentIndex, getPlayMode, getCurrentSong } from '../../../store/selectors/player.selector';
 import { Song } from '../../../services/data-types/common.types';
 import { PlayMode } from './player-type';
+import { SetCurrentIndex } from 'src/app/store/actions/player.actions';
 
 @Component({
   selector: 'app-wy-player',
@@ -20,6 +21,12 @@ export class WyPlayerComponent implements OnInit {
   playList: Song[];
   currentIndex: number;
   currentSong: Song;
+
+  // 播放状态
+  playing = false;
+
+  // 是否可以播放
+  songReady = false;
 
   @ViewChild('audio', { static: true }) private audio: ElementRef;
   private audioEl: HTMLAudioElement;
@@ -80,20 +87,64 @@ export class WyPlayerComponent implements OnInit {
 
 
   onCanplay() {
+    this.songReady = true; // 可以播放
     this.play();
   }
 
   private play() {
     this.audioEl.play();
+    this.playing = true; // 正在播放
   }
 
   get playUrl() {
     return this.currentSong ? this.currentSong.al.picUrl : '//s4.music.126.net/style/web2/img/default/default_album.jpg';
   }
 
+  // 获取当前播放时间
   onTimeUpdate(e: Event) {
-    console.log(e);
     this.currentTime = (<HTMLAudioElement>e.target).currentTime;
+  }
 
+  // 切换播放/暂停
+  onToggle() {
+    if (!this.currentSong) {
+      if (this.playList.length) {
+        this.updateIndex(0);
+      }
+    } else {
+      if (this.songReady) {
+        this.playing = !this.playing;
+        this.playing ? this.audioEl.play() : this.audioEl.pause();
+      }
+    }
+  }
+
+  // 上一首/下一首
+  switchSong(index: number, type: string) {
+    if (!this.songReady) return;
+    if (this.playList.length === 1) { // 播放列表只有一首歌
+      this.loop();
+    } else {
+      let newIndex;
+      if (type === 'onNext') { // 下一首
+        console.log('onNext');
+        newIndex = index >= this.playList.length ? 0 : index;
+      } else if (type === 'onPrev') { // 上一首
+        console.log('onPrev');
+        newIndex = index < 0 ? this.playList.length - 1 : index;
+      }
+      this.updateIndex(newIndex);
+    }
+  }
+
+  // 更新当前播放的歌曲索引，以及变为 不可播放状态
+  private updateIndex(index) {
+    this.store$.dispatch(SetCurrentIndex({ currentIndex: index }));
+    this.songReady = false;
+  }
+
+  private loop() {
+    this.audioEl.currentTime = 0;
+    this.play();
   }
 }
